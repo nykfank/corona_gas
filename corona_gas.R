15d6e13878ab9d27ec4b5e486bd117cc8deaf4b2
nykfank <nick@nyk.ch>
2020-04-05 02:08:29 +0200

    Update corona_gas.R

--- corona_gas.R
+++ corona_gas.R
@@ -241,7 +241,8 @@ html <- sprintf('<html><head><title>COVID-19 pandemic visualisation by Niklaus F
 <p>The idea is to represent the spread of the pandemic as ideal gases inside each country.
 Dots represents 1000 infected (green) or dead (red).</p>
 <p>Based on data from <i>2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE.</i></p>
-</body></html>', videofile)
+<p>Updated on %s</p>
+</body></html>', videofile, Sys.time())
 write(html, file="corona.html")
 
 # Upload to webserver
